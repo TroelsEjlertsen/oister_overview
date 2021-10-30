0b10c53fbd5de42cfe53e7c8ed47dc84b6d7fe67
TroelsEjlertsen <ejlertsen93@gmail.com>
2021-10-30 18:42:46 +0200

    added GB/MB radio button

--- functions.py
+++ functions.py
@@ -9,36 +9,64 @@ import pandas as pd
 import tabula
 import camelot
 import numpy as np
-
+import glob, os
+from pikepdf import Pdf
 #%% functions
 
-def load_data(pdf):
-    tables = camelot.read_pdf(pdf, flavor='stream',
+def read_pdfs():
+   # Change directory to data folder
+   os.chdir("data/")
+   
+   # read PDF names
+   pdfs = [file for file in glob.glob("*.pdf")]
+   
+   # repair corrupted PDF
+   for name in pdfs:
+       with Pdf.open(name, allow_overwriting_input=True) as pdf:
+           pdf.save(name)
+  
+   return pdfs 
+      
+
+
+def make_table(pdf):
+    
+    table = camelot.read_pdf(pdf, flavor='stream',
                           pages='all',
                           strip_text='Data',
                           row_tol=10,
                           #columns=['100'],
                            split_text = True)
+    return table
+    
+def make_tables():
+    pdfs = read_pdfs()
+    
+    tables = {"{}".format(pdf): make_table(pdf) for pdf in pdfs}
+    
+    #return to main directory
+    os.chdir("../")
+    
     return tables
-    
-    
     
     
 def sort_column(df):
     
     df1 = df[df.iloc[:,2].str.contains("KB")]
     df2 = df[df.iloc[:,2].str.contains("MB")]
-    #df3 = df[[0,4]]
-    
+
     df4 = df1.append(df2)
+      
     
-    
-    
-    
-    if df[4].str.contains("Vrighed\nE").any():
+    if df[4].str.contains("Vrighed\nE").any() or df[4].str.contains("Vrighed").any():
         df3 = df[[0,1,4]]
         df3 = df3.rename(columns = {4:"call_time"})
-        df3["call_time"] = df3["call_time"].str.strip("Vrighed\nE")
+        
+        if df[4].str.contains("Vrighed\nE").any():
+            df3["call_time"] = df3["call_time"].str.strip("Vrighed\nE")
+        else:
+            df3["call_time"] = df3["call_time"].str.strip("Vrighed")
+            
         df3["call_time"] = pd.to_timedelta(df3["call_time"], errors = 'ignore')
         df3["call_time"] = df3["call_time"].fillna(pd.Timedelta("0 days 00:00:00"))
         
@@ -46,10 +74,7 @@ def sort_column(df):
         
     else:
         df3 = 0
-    
-    
-    
-    
+      
     #keep index in ascending order
     df4 = df4.sort_index()
     
--- main.py
+++ main.py
@@ -11,53 +11,58 @@ import camelot
 import numpy as np
 import os
 import streamlit as st
-from functions import load_data
-from functions import sort_column
+from functions import read_pdfs,make_table, make_tables, sort_column
+import glob, os
 
 
-# %% READ PDF
+#%% IF df is not saved to csv file
 
-new_name = ["2021_jan","2021_feb", "2021_mar","2021_apr","2021_may","2021_jun","2021_jul","2021_aug","2021_sep"]
-
-pdf_name = ["data/2021-0{}.pdf".format(m+1) for m in range(0,10)]
-pdf_name[-1] = "data/2021-10.pdf"
-
-tables = {"{}".format(new_name[i]): load_data(pdf_name[i]) for i in range(0,len(new_name))}
-
-
-#%% extract tables
-
-data = []
-
-for tab in tables:
-    #print(tables[tab][0].df)
-    for i in range(0,len(tables[tab])):
-        #print(tab[i])
-        data.append(tables[tab][i].df) 
-
-#%% Sort dataframes
-df = pd.DataFrame()
-
-for dat in data:
-    #print(dat)
-    df = df.append(sort_column(dat))
+if not os.path.isfile("data.csv"): 
     
-# Reset index
-df = df.reset_index(drop=True) 
+    # %% READ PDF
+    tables = make_tables()    
+    
+    data = []
+    
+    for tab in tables:
+        #print(tables[tab][0].df)
+        for i in range(0,len(tables[tab])):
+            #print(tab[i])
+            data.append(tables[tab][i].df) 
+    
+    #%% Sort dataframes
+    df = pd.DataFrame()
+    
+    for dat in data:
+        #print(dat)
+        df = df.append(sort_column(dat))
+        
+    #%%     
+    # Reset index
+    df = df.reset_index(drop=True) 
+    
+    #Make datetime
+    df["date"] = pd.to_datetime(df[0]+ " " + df[1], errors = 'coerce', dayfirst = True)
+    
+    #replace NAT with zeros
+    df["call_time"] = df["call_time"].fillna(pd.Timedelta("0 days 00:00:00"))
+    
+    #drop  columns
+    # df = df.drop(columns = [0,1,3,6,5,7,8])
+    df = df[["date","Data[MB]","call_time"]]
+    # set date as index
+    df = df.set_index("date")
 
-#Make datetime
-df["date"] = pd.to_datetime(df[0]+ " " + df[1], errors = 'coerce', dayfirst = True)
+    # Save df to CSV
+    df.to_csv("data.csv")
 
-#replace NAT with zeros
-df["call_time"] = df["call_time"].fillna(pd.Timedelta("0 days 00:00:00"))
-
-#drop  columns
-# df = df.drop(columns = [0,1,3,6,5,7,8])
-df = df[["date","Data[MB]","call_time"]]
-# set date as index
-df = df.set_index("date")
-
-
+#%% IF df is saved to csv file
+else:
+    
+    df = pd.read_csv("data.csv")
+    df["date"] = pd.to_datetime(df["date"])
+    df["call_time"] = pd.to_timedelta(df["call_time"])
+    df = df.set_index("date")
 
 #%% Analyse data
 # group by monthly sum
@@ -67,39 +72,110 @@ df1 = df["Data[MB]"].groupby(pd.Grouper(freq="M")).sum()
 # call time sum
 df2 = df["call_time"].groupby(pd.Grouper(freq="M")).sum()
 df2 = df2.rename("Call time[min.]")
+df3 = df2.rename("Call time[hr]")
+df4 = df1.rename("Data[GB]")
+df4 = df4/1000
+
 #convert to minutes
 df2 = df2.astype('timedelta64[m]')
+df3 = df3.astype('timedelta64[h]')
+
+#avg. values
+d = {"Avg. monthly Call time[h]":df3.mean(), "Avg. monthly data[GB]": df1.mean()/1000,
+     "Max. call time[h]":df3.max(),
+     "Max. Data use[GB]":df1.max()/1000}
+df_avg = pd.DataFrame(data = d, index=[0])
+
+if df3.mean() < 1:
+    df_avg.iloc[0,0] = df2.mean()
+    df_avg.iloc[0,2] = df2.max()
+    df_avg = df_avg.rename(columns = {"Avg. monthly Call time[h]":"Avg. monthly Call time[min.]",
+                                       "Max. call time[h]": "Max. call time[min.]"})
 
 #%% Display using Streamlit
 
+# G number of years
 yr_range = [int(df.index.year.min()), int(df.index.year.max())]
+
 st.title("Mobile data Usage for {}-{}".format(yr_range[0], yr_range[1]))
 
-st.header("Monthly Data usage")
-st.bar_chart(df1)
 
-st.header("Monthly Call time")
-st.bar_chart(df2)
-
-st.header("Overall Data Usage")
-st.line_chart(df[["Data[MB]"]])
-
-# st.header("Overall Call Usage")
-# st.line_chart(df[["call_time"]])
-
-
-@st.cache
-def disp_data(df):
+@st.cache#(persistent=True)
+def load_df(df):
     return df
 
-# show raw data
-if st.checkbox("See raw data"):   
-    
-    'data', disp_data(df)   
-    
-    
+df_mean = load_df(df_avg)
+df_data = load_df(df["Data[MB]"])
+df_call = load_df(df["call_time"])
+df11 = load_df(df1)
+df22 = load_df(df2)
+df33 = load_df(df3)
+df44 = load_df(df4)
+
+#%% Show stats in a table
+
+st.header("Average and max. monthly usage")
+st.table(df_mean)
+
+#%% 
+st.header("Overall data usage")
+st.line_chart(df_data)
+
+#%% Initialize session state
+if "unit" not in st.session_state:
+    st.session_state.unit = "MB"
+if "data_df" not in st.session_state:
+    st.session_state.data_df = df11    
+if "item" not in st.session_state:
+    st.session_state.item = "hr."
+if "df" not in st.session_state:
+    st.session_state.df = df33    
     
 
+#%% Monthly data usage
+st.header("Monthly Data usage")   
+def handle_unit():
+    if st.session_state.new_unit:
+        st.session_state.unit = st.session_state.new_unit
+        
+        if st.session_state.unit == "[GB]":
+            st.session_state.data_df = df44
+        else:
+            st.session_state.data_df = df11
+            
+            
+unit_items = ["[MB]", "[GB]"]    
+radio2 = st.radio("[MB]/[GB]", unit_items, on_change=handle_unit, key="new_unit")
+
+st.bar_chart(st.session_state.data_df)
+
+
+# st.header("Overall call time usage")
+# st.line_chart(df_call)
+
+#%% Monthly call time
+st.header("Monthly Call time")
+
+
+def handle_change():
+    if st.session_state.new_item:
+        st.session_state["item"] = st.session_state.new_item
+        
+        if st.session_state.item == "min.":
+            st.session_state.df = df22
+        else:
+            st.session_state.df = df33
+            
+        
+        
+
+radio_items = ["hr.", "min."]
+radio2 = st.radio("hr./min.", radio_items,
+                 on_change=handle_change,
+                 key = "new_item")
+
+
+st.bar_chart(st.session_state.df)
 
 
                    
